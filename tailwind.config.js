454f95aa22ba22522abc47cf4f23fab337a3e071
Kéwan B <mail@kewan.fr>
2024-12-30 03:08:21 +0100

    Code Base

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.php",
+    "./**/*.html",
+    "index.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
